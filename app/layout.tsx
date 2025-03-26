9c19a3270a00715cb5b636c25da0d27edb41f487
Krlostks <carlospixel13@gmail.com>
2025-03-25 21:00:25 -0600

    Add Property from nextjs

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import NavBar from "./components/navbar/Navbar";
 import Modal from "./components/modals/Modal";
 import LoginModal from "./components/modals/LoginModal";
 import SignupModal from "./components/modals/SignupModal";
+import AddPropertyModal from "./components/modals/AddPropertyModal";
 
 const inter = Inter({
   subsets: ["latin"]
@@ -33,6 +34,7 @@ export default function RootLayout({
         </div>
         <LoginModal/>
         <SignupModal/>
+        <AddPropertyModal/>
       </body>
     </html>
   );
